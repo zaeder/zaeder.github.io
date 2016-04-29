5e627718c0c434ec037b2d0dbc1e170242f75e12
Florian THIERRY <zaeder@live.fr>
2016-04-29 10:19:25 +0200

    Fix script for logos

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-jQuery('body').ready(function(){
+var init = function(){
     jQuery('body').css('min-height', jQuery(window).height());
     
     articles = jQuery('div#infos section#main_content article');
@@ -6,14 +6,12 @@ jQuery('body').ready(function(){
         jQuery(articles[articles.length - 1]).attr('id', 'single');
     }
     
-    jQuery('div#infos section#main_content article div.logo').each(function(){
-        jQuery(this).load(function(){
-            divHeight = jQuery(this).height();
-            imgHeight = jQuery(this).find('img').height();
-            marginTop = Math.ceil((divHeight - imgHeight) / 2);
-            if(marginTop > 0){
-                jQuery(this).find('img').css('margin-top', marginTop);
-            }
-        });
+    jQuery('div#infos section#main_content article div.logo img').each(function(){
+        divHeight = jQuery(this).parent().height();
+        imgHeight = jQuery(this).height();
+        marginTop = Math.ceil((divHeight - imgHeight) / 2);
+        if(marginTop > 0){
+            jQuery(this).css('margin-top', marginTop);
+        }
     });
-});
+};
