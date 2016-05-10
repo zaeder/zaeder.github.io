26720c08bd20083d5c896e3f5c1db215b5e0083c
Florian THIERRY <zaeder@live.fr>
2016-05-10 13:13:04 +0200

    Add jsLogger to main page

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 var init = function(){
+    
+    var setSameHeight = function(start){
+        articles = jQuery('div#infos section#main_content article');
+        for(loop = start, len = articles.length; loop < len; loop+=2){
+            if(jQuery(articles[loop]).height() >= jQuery(articles[loop+1]).height()){
+                jQuery(articles[loop+1]).height(jQuery(articles[loop]).height());
+            } else {
+                jQuery(articles[loop]).height(jQuery(articles[loop+1]).height());
+            }
+        }
+    }
+    
     jQuery('body').css('min-height', jQuery(window).height());
     
     articles = jQuery('div#infos section#main_content article');
     if(articles.length % 2 !== 0){
-        jQuery(articles[articles.length - 1]).attr('id', 'single');
+        jQuery(articles[0]).attr('id', 'single');
+        setSameHeight(1);
+    } else {
+        setSameHeight(0);
     }
     
     jQuery('div#infos section#main_content article div.logo img').each(function(){
